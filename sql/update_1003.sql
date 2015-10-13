88f0e4e2973c913ce15ce17be731f44c16e802cb
Jaap Jansma <jaap@edeveloper.nl>
2015-10-13 15:58:09 +0200

    removed database name

--- sql/update_1003.sql
+++ sql/update_1003.sql
@@ -1,5 +1,5 @@
-UPDATE `civirules`.`civirule_trigger` SET `class_name`='CRM_CivirulesPostTrigger_Activity' WHERE `class_name` = 'CRM_CivirulesPostEvent_Activity';
-UPDATE `civirules`.`civirule_trigger` SET `class_name`='CRM_CivirulesPostTrigger_Contact' WHERE `class_name` = 'CRM_CivirulesPostEvent_Contact';
-UPDATE `civirules`.`civirule_trigger` SET `class_name`='CRM_CivirulesPostTrigger_GroupContact' WHERE `class_name` = 'CRM_CivirulesPostEvent_GroupContact';
-UPDATE `civirules`.`civirule_trigger` SET `class_name`='CRM_CivirulesCronTrigger_Birthday' WHERE `class_name` = 'CRM_CivirulesCronEvent_Birthday';
-UPDATE `civirules`.`civirule_trigger` SET `class_name`='CRM_CivirulesCronTrigger_GroupMembership' WHERE `class_name` = 'CRM_CivirulesCronEvent_GroupMembership';
+UPDATE `civirule_trigger` SET `class_name`='CRM_CivirulesPostTrigger_Activity' WHERE `class_name` = 'CRM_CivirulesPostEvent_Activity';
+UPDATE `civirule_trigger` SET `class_name`='CRM_CivirulesPostTrigger_Contact' WHERE `class_name` = 'CRM_CivirulesPostEvent_Contact';
+UPDATE `civirule_trigger` SET `class_name`='CRM_CivirulesPostTrigger_GroupContact' WHERE `class_name` = 'CRM_CivirulesPostEvent_GroupContact';
+UPDATE `civirule_trigger` SET `class_name`='CRM_CivirulesCronTrigger_Birthday' WHERE `class_name` = 'CRM_CivirulesCronEvent_Birthday';
+UPDATE `civirule_trigger` SET `class_name`='CRM_CivirulesCronTrigger_GroupMembership' WHERE `class_name` = 'CRM_CivirulesCronTrigger_GroupMembership';
