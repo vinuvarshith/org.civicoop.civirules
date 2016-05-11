0e7027babb9901f7627a8aeb3241214af05bc3d2
systopia <endres@systopia.de>
2016-05-11 21:47:16 +0200

    change big select elements to select2

--- CRM/CivirulesConditions/Form/FieldValueComparison.php
+++ CRM/CivirulesConditions/Form/FieldValueComparison.php
@@ -110,7 +110,7 @@ class CRM_CivirulesConditions_Form_FieldValueComparison extends CRM_CivirulesCon
 
     $this->add('hidden', 'rule_condition_id');
     $this->add('select', 'entity', ts('Entity'), $this->getEntityOptions(), true);
-    $this->add('select', 'field', ts('Field'), $this->getFields(), true);
+    $this->add('select', 'field', ts('Field'), $this->getFields(), true, array('class' => 'crm-select2'));
     $this->assign('entities', $this->getEntities());
     $this->assign('custom_field_multi_select_html_types', CRM_Civirules_Utils_CustomField::getMultiselectTypes());
   }
