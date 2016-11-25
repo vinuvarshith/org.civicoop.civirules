69a8b3cfad10449b00866c5c2ba66dccd23d9c4d
Erik Hommel <hommel@ee-atwork.nl>
2016-11-25 16:15:35 +0100

    fix #114 and #117

--- sql/uninstall.sql
+++ sql/uninstall.sql
@@ -1,7 +1,9 @@
 DROP TABLE IF EXISTS `civirule_rule_action`;
+DROP TABLE IF EXISTS `civirule_rule_tag`;
 DROP TABLE IF EXISTS `civirule_rule_condition`;
 DROP TABLE IF EXISTS `civirule_rule_log`;
 DROP TABLE IF EXISTS `civirule_rule`;
 DROP TABLE IF EXISTS `civirule_action`;
 DROP TABLE IF EXISTS `civirule_condition`;
 DROP TABLE IF EXISTS `civirule_trigger`;
+DELETE FROM civicrm_managed WHERE module = 'org.civicoop.civirules';
