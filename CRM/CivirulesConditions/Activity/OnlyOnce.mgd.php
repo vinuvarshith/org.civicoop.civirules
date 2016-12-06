80b126d07c128b32013b1047cbac88cbbd16a903
Erik Hommel <hommel@ee-atwork.nl>
2016-12-06 12:02:38 +0100

    add condition onlyonce for activity

---
+++ CRM/CivirulesConditions/Activity/OnlyOnce.mgd.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Erik Hommel (CiviCooP) <erik.hommel@civicoop.org>
+ * @license http://www.gnu.org/licenses/agpl-3.0.html
+ */
+return array (
+  0 =>
+    array (
+      'name' => 'Civirules:Condition.OnlyOnce',
+      'entity' => 'CiviRuleCondition',
+      'params' =>
+        array (
+          'version' => 3,
+          'name' => 'once_for_activity',
+          'label' => 'Activity: execute action only once no matter what the activity contacts',
+          'class_name' => 'CRM_CivirulesConditions_Activity_OnlyOnce',
+          'is_active' => 1
+        ),
+    ),
+);
