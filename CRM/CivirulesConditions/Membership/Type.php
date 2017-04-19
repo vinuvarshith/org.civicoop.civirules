c02d1f9c666be16387a0e3f643c4d375b4eb08c1
Matthew Wire <devel@mrwire.co.uk>
2017-04-19 12:14:02 +0000

    Remove extra bracket

--- CRM/CivirulesConditions/Membership/Type.php
+++ CRM/CivirulesConditions/Membership/Type.php
@@ -69,7 +69,7 @@ class CRM_CivirulesConditions_Membership_Type extends CRM_Civirules_Condition {
       $params = array(
         'is_active' => 1,
         'options' => array('limit' => 0, 'sort' => "name ASC"),
-      ));
+      );
       $membershipTypes = civicrm_api3('MembershipType', 'Get', $params);
       $operator = null;
       if ($this->conditionParams['operator'] == 0) {
