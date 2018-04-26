91b87e562c5399826ffa65080599fa422d5184dc
Erik Hommel <hommel@ee-atwork.nl>
2018-04-26 20:33:24 +0200

    fix #211 - add condition has (never) been in group ;

---
+++ CRM/CivirulesConditions/Contact/HasBeenInGroup.mgd.php
@@ -0,0 +1,17 @@
+<?php
+
+return array (
+  0 =>
+    array (
+      'name' => 'Civirules:Condition.Contact.HasBeenInGroup',
+      'entity' => 'CiviRuleCondition',
+      'params' =>
+        array (
+          'version' => 3,
+          'name' => 'contact_has_been_in_group',
+          'label' => 'Has (never) been in group',
+          'class_name' => 'CRM_CivirulesConditions_Contact_HasBeenInGroup',
+          'is_active' => 1
+        ),
+    ),
+);
