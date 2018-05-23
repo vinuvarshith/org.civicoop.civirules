e75ef7aed10dff02ac5d021cc302a2532ae87e46
Jaap Jansma <jaap@edeveloper.nl>
2018-05-23 16:09:52 +0200

    Fixed #219: added condition contact has membership

---
+++ CRM/CivirulesConditions/Membership/ContactHasMembership.mgd.php
@@ -0,0 +1,17 @@
+<?php
+
+return array (
+  0 =>
+    array (
+      'name' => 'Civirules:Condition.ContactHasMembership',
+      'entity' => 'CiviRuleCondition',
+      'params' =>
+        array (
+          'version' => 3,
+          'name' => 'contacthasmembership',
+          'label' => 'Contact has membership',
+          'class_name' => 'CRM_CivirulesConditions_Membership_ContactHasMembership',
+          'is_active' => 1
+        ),
+    ),
+);
